8052007fd45540ee3bd790febb5fa88fd32ea039
Lucas Vaini <125685038+lucasvaini@users.noreply.github.com>
2023-04-14 13:54:38 -0300

    Add files via upload

---
+++ aula1.java
@@ -0,0 +1,21 @@
+// -> É um comentário de 1 linha
+
+
+/* 
+Esse é o nosso primeiro programa em Java
+Ele fará a impressão de uma mensagem na tela
+*/
+
+public class aula1{
+    
+//Metodo main: inicia a nossa aplicação Java
+
+public static void main(String args[]) {
+
+    System.out.println("Ola");
+    System.out.println("AMO");
+    System.out.println("JAVA");
+
+}
+
+}
