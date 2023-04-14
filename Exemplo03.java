bed225364e090d4fde77dafbf41ab6fd02972450
Lucas Vaini <125685038+lucasvaini@users.noreply.github.com>
2023-04-14 13:41:32 -0300

    Add files via upload

---
+++ Exemplo03.java
@@ -0,0 +1,17 @@
+// Cálculo da área do Triângulo
+
+
+public class Exemplo03 {
+
+    public static final double BASE = 5.5;
+    
+
+    public static void main(String[] args) {
+        
+        double altura = 4.0;
+        double area = (BASE * altura)/2;
+        System.out.println("A area do triângulo é: " + area + " cm²");
+
+    }
+
+}
